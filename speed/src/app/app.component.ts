fe82cbec4fa2b37d60575e4b31dcf7813ebf29c0
Jago <jago84@gmail.com>
2018-12-12 22:51:36 +0100

    Añadida comprobación de nueva versión

--- speed/src/app/app.component.ts
+++ speed/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'speed';
+
+  constructor(
+    private swUpdate: SwUpdate
+  ) {}
+
+  ngOnInit() {
+    this.checkNewVersion();
+  }
+
+  private checkNewVersion() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available! Update?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
