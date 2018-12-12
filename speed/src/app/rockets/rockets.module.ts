f73e6eee430030ef192d5b5d639c244c305dd5c6
Jago <jago84@gmail.com>
2018-12-12 19:13:58 +0100

    Creado módulo Rockets

---
+++ speed/src/app/rockets/rockets.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RocketsRoutingModule } from './rockets-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RocketsRoutingModule
+  ],
+  declarations: []
+})
+export class RocketsModule { }
