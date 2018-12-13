8000a79a77cba7e16868c2883005d86257ec7bd7
Jago <jago84@gmail.com>
2018-12-13 14:35:27 +0100

    Lógica y presentación de la página de detalle de lanzamiento

--- speed/src/app/rockets/launches-page/launches-page.component.ts
+++ speed/src/app/rockets/launches-page/launches-page.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Launch } from '../../models/launch';
 import { ActivatedRoute } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { State } from '../../reducers';
-import { map } from 'rxjs/operators';
 import { TitleService } from '../../title.service';
+import { LoadLaunchs } from '../../reducers/launch.actions';
 
 @Component({
   selector: 'app-launches-page',
@@ -23,12 +23,23 @@ export class LaunchesPageComponent implements OnInit {
   launch: Launch;
 
   ngOnInit() {
+    this.loadValues();
+    this.observeValues();
+  }
+
+  loadValues() {
+    this.globalStore.dispatch(new LoadLaunchs());
+  }
+
+  observeValues() {
     const launches$ = this.globalStore.select('launch');
     const routeParams$ = this.route.params;
-    forkJoin([routeParams$, launches$])
+    combineLatest(routeParams$, launches$)
       .subscribe(([{ launch: launchId }, { launches }]) => {
-        this.launch = launches.find(launch => launch.id === launchId);
-        this.titleService.setTitle(`Lanzamiento: ${this.launch.name}`);
+        if (launches.length) {
+          this.launch = launches.find(launch => launch.id === +launchId);
+          this.titleService.setTitle(`Lanzamiento: ${this.launch.name}`);
+        }
       });
   }
 
