b7af626fce680cc49ade385ddce379ff439bd356
Jago <jago84@gmail.com>
2018-12-13 13:09:46 +0100

    Faltaba el effect de los estados

--- speed/src/app/reducers/status.effects.ts
+++ speed/src/app/reducers/status.effects.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import { ProviderService } from '../provider.service';
+import { StatusActionTypes, StatusesLoaded } from './status.actions';
+import { mergeMap, map } from 'rxjs/operators';
 
 
 @Injectable()
 export class StatusEffects {
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private provider: ProviderService) {}
+
+  @Effect() save$ = this.actions$.ofType(StatusActionTypes.LoadStatuss).pipe(
+    mergeMap(() => this.provider.getStatuses()),
+    map(launches => new StatusesLoaded(launches))
+  );
 }
